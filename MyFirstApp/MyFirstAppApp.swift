1aa876180a4069059178a7092a0daad31448b910
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-03-02 21:44:53 +0900

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by james on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
